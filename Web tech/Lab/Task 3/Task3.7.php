f631f1b2b469dfd46fcf61a94fe8cc655062c238
Bishal Paul <123397462+Bpbishal@users.noreply.github.com>
2024-11-15 18:48:38 +0600

    Add files via upload

---
+++ Web tech/Lab/Task 3/Task3.7.php
@@ -0,0 +1,36 @@
+<?php
+      for($i=1;$i<=3;$i++)
+      {
+          for($j=1;$j<=$i;$j++)
+          {
+              print("* ");
+          }
+          print("<br>");
+
+      }
+      print("<br>");
+
+     
+      for($i=3;$i>=1;$i--)
+      {
+          for($j=1;$j<=$i;$j++)
+          {
+              print($j." ");
+          }
+          print("<br>");
+
+      }
+      print("<br>");
+
+      $letter="A";
+      for($i=1;$i<=3;$i++)
+      {
+          for($j=1;$j<=$i;$j++)
+          {
+              print($letter." ");
+              $letter++;
+          }
+          print("<br>");
+
+      }
+?>
